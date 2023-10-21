310d7e18dfcf40df923d316f8369c8c342362653
Sok Kim Thanh <thanhsk1991@gmail.com>
2023-10-21 21:14:29 +0700

    Update BT17.cs

--- LTUD1_BT_WINFORM/BT_NANGCAO/BT17.cs
+++ LTUD1_BT_WINFORM/BT_NANGCAO/BT17.cs
@@ -33,25 +33,31 @@ namespace BT8_LISTBOX.BT_NANGCAO
 
         private void BT17_Load(object sender, EventArgs e)
         {
-
+            // tạo từ điển
             dictionary = new Dictionary<string, int>()
             {
                 { "Khu vực 1", 50  },//index 0
                 { "Khu vực 2", 100 },//index 1
                 { "Khu vực 3", 100 }// index 2 
             };
-
+            // tạo mới dữ liệu gán
             bindingSource = new BindingSource(dictionary, null);
-            cboKhuVuc.DataSource = bindingSource;
-            cboKhuVuc.DisplayMember = "Key";
-            cboKhuVuc.ValueMember = "Value";
 
+            // gán dữ liệu vào combobox 
+            cboKhuVuc.DataSource = bindingSource;
+            // tên của khu vực 
+            cboKhuVuc.DisplayMember = "Key";
+            // giá trị của khu vực
+            cboKhuVuc.ValueMember = "Value";
+            // mặc định chọn 
             cboKhuVuc.SelectedIndex = 0;//"Khu vực 1"
+            // gán giá trị định mức 
             txtDinhMuc.Text = cboKhuVuc.SelectedValue!.ToString();// value 50;
 
+            // bắt lỗi cụ thể required field
             controlList = new List<Control> { txtSoCu, txtSoMoi, txtHoTen, cboKhuVuc };
 
-            //required tất cả các field
+            //required tất cả các field đc chọn
             //f.ValidateInputs(controlList);
         }
 
@@ -59,6 +65,7 @@ namespace BT8_LISTBOX.BT_NANGCAO
         {
             ComboBox cboSetDinhMuc = (ComboBox)sender;
             txtDinhMuc.Text = cboSetDinhMuc.SelectedValue!.ToString();
+
         }
 
         private void txtSoCu_TextChanged(object sender, EventArgs e)
@@ -89,9 +96,7 @@ namespace BT8_LISTBOX.BT_NANGCAO
         {
             if (f.IsValidateInputs(controlList))
             {
-                r.LblHint.Text = "Nhập OK!";
-                r.PnError.BackColor = Color.Green;
-
+                // chặn số âm khi nhập chỉ số cũ và mới
                 double soMoi = double.Parse(txtSoMoi.Text.ToString());
                 double soCu = double.Parse(txtSoCu.Text.ToString());
                 if (soCu >= soMoi)
@@ -100,23 +105,40 @@ namespace BT8_LISTBOX.BT_NANGCAO
                     MessageBox.Show("Chỉ số mới phải lớn hơn hoặc bằng chỉ số cũ!");
                     txtSoMoi.Text = string.Empty;
                     txtSoMoi.Focus();
+                    r.LblHint.Text = "Chặn số âm!";
+                    r.PnError.BackColor = ErrColors.mes_black;
                     return;
                 }
+
+                // tính chỉ số tiêu thụ, đơn giá, thành tiền
                 double tieuthu = (soMoi - soCu);
-                bool isNgoaiDinhMuc = int.Parse(txtTieuThu.Text.ToString()) > Convert.ToInt32(cboKhuVuc.SelectedValue);
+                bool isNgoaiDinhMuc = tieuthu > Convert.ToInt32(cboKhuVuc.SelectedValue);
                 int dongia = isNgoaiDinhMuc ? 1000 : 500;
                 double thanhtien = dongia * tieuthu;
 
+                // Định dạng số tiền
                 txtTieuThu.Text = tieuthu.ToString();
                 txtThanhTien.Text = thanhtien.ToString("C");
 
+                // khởi tạo item cho listview
                 ListViewItem lvitem = new ListViewItem();
+
+                // thêm item đầu tiên là họ tên
                 lvitem.Text = txtHoTen.Text.ToString();
+
                 // Lấy giá trị của Key khi chọn một mục trong ComboBox  
-                lvitem.SubItems.Add(cboKhuVuc.SelectedItem.ToString());
+                lvitem.SubItems.Add(cboKhuVuc.Text);
+
+                // thêm định mức
                 lvitem.SubItems.Add(txtDinhMuc.Text);
+
+                // thêm tiêu thụ
                 lvitem.SubItems.Add(tieuthu.ToString());
+
+                // thêm thành tiền
                 lvitem.SubItems.Add(thanhtien.ToString());
+
+                // thêm item vào listview
                 lvBCTTD.Items.Add(lvitem);
 
 
@@ -127,11 +149,15 @@ namespace BT8_LISTBOX.BT_NANGCAO
                     tongtien += double.Parse(lvBCTTD.Items[i].SubItems[4].Text.ToString());
                 }
                 txtTongTien.Text = tongtien.ToString("C");
+
+                // thông báo thêm thành công
+                r.LblHint.Text = "Thêm thành công!";
+                r.PnError.BackColor = ErrColors.mes_success;
             }
             else
             {
                 r.LblHint.Text = "Vui lòng nhập giá trị hợp lệ.";
-                r.PnError.BackColor = Color.Red;
+                r.PnError.BackColor = ErrColors.mes_error;
             }
         }
 
@@ -147,29 +173,40 @@ namespace BT8_LISTBOX.BT_NANGCAO
 
         private void btnXoa_Click(object sender, EventArgs e)
         {
+            // bắt lỗi chưa chọn dòng
             try
             {
+                // số dòng được chọn
                 int row = lvBCTTD.SelectedIndices[0];
-                DialogResult r = MessageBox.Show("Bạn có muốn xóa dữ liệu hay không?", "Xác nhận xóa", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-                if (r == DialogResult.Yes)
+
+                // hộp thông báo 
+                DialogResult result = MessageBox.Show("Bạn có muốn xóa dữ liệu hay không?", "Xác nhận xóa", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
                 {
                     lvBCTTD.Items.RemoveAt(row);
+
                 }
                 // tính tổng tiền đã trả theo tháng
-                tongtien = 0;
+                tongtien = 0;// reset tổng tiền để cập nhật tổng tiền mới 
                 for (int i = 0; i < lvBCTTD.Items.Count; i++)
                 {
                     tongtien += double.Parse(lvBCTTD.Items[i].SubItems[4].Text.ToString());
                 }
+                // định dạng tổng tiền
                 txtTongTien.Text = tongtien.ToString("C");
+
+                // thông báo xóa thành công 
+                r.LblHint.Text = "Xóa thành công!";
+                r.PnError.BackColor = ErrColors.mes_success;
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Vui lòng chọn dòng để xóa!");
+                // thông báo chưa chọn dòng để xóa
+                r.LblHint.Text = "Vui lòng chọn dòng để xóa!";
+                r.PnError.BackColor = ErrColors.mes_error;
+                Console.WriteLine(ex.Message);
             }
         }
-
-
 
         private void BT17_KeyDown(object sender, KeyEventArgs e)
         {
@@ -193,7 +230,11 @@ namespace BT8_LISTBOX.BT_NANGCAO
         {
             r.NoSymNumPress(e);
         }
-
+        /// <summary>
+        /// Nhập mới các trường dữ liệu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnNhapMoi_Click(object sender, EventArgs e)
         {
             txtHoTen.Text = string.Empty;
@@ -205,18 +246,23 @@ namespace BT8_LISTBOX.BT_NANGCAO
             txtHoTen.Focus();
         }
 
+        /// <summary>
+        /// Chặn rời đi khi chưa nhập input required
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ValidateInputs_Leave(object sender, EventArgs e)
         {
             Control control = (Control)sender;
             if (f.ValidateInput(control))
             {
-                r.PnError.BackColor = Color.Black;
+                r.PnError.BackColor = ErrColors.mes_black;
                 r.LblHint.Text = "Vui lòng nhập giá trị hợp lệ.";
                 control.Focus();
             }
             else
             {
-                r.PnError.BackColor = Color.Green;
+                r.PnError.BackColor = ErrColors.mes_success;
                 r.LblHint.Text = "Nhập OK!";
             }
         }
